28d4a10ffdd13e1e977196737befda344b3003c0
Brian Bouchard <b.bouch@gmail.com>
2020-05-02 12:33:06 -0600

    Filter restaurants by genre

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import Grid from './components/Grid';
 import Filter from './components/Filter';
-import EmptyGrid from './components/EmptyGrid'
+import EmptyGrid from './components/EmptyGrid';
 
 import './App.css';
 import config from './data/config';
 
 const App = () => {
   const [restaurants, setRestaurants] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [currentState, setCurrentState] = useState('ShowAll');
+  const [currentGenre, setCurrentGenre] = useState('ShowAll');
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
 
   useEffect(() => {
@@ -22,36 +25,92 @@ const App = () => {
       )
         .then((response) => response.json())
         .then((data) => data.sort((a, b) => (a.name > b.name ? 1 : -1)))
-        .then((data) => {
-          setRestaurants(data);
-          setFilteredRestaurants(data);
+        .then((sortedData) => {
+          let allGenres = [];
+          sortedData.forEach((restaurant) => {
+            let restaurantGenres = restaurant.genre.split(',');
+            restaurantGenres.forEach((genre) => {
+              if (!allGenres.includes(genre)) {
+                allGenres.push(genre);
+              }
+            });
+            allGenres.sort((a, b) => (a > b ? 1 : -1));
+            setGenres([...allGenres]);
+          });
+          return sortedData;
+        })
+        .then((sortedData) => {
+          setRestaurants(sortedData);
+          setFilteredRestaurants(sortedData);
         });
     }
     fetchData();
-  }, [setRestaurants, setFilteredRestaurants]);
+  }, [setRestaurants, setFilteredRestaurants, setGenres]);
 
   const handleStateAbbr = (e) => {
     const stateAbbr = e.target.value;
-    const filteredRestaurants = restaurants.filter((restaurant) => {
-      return restaurant.state === stateAbbr;
-    });
+    setCurrentState(stateAbbr);
+    resetFilters('states', stateAbbr);
+  };
 
-    if (stateAbbr === 'ShowAll') {
-      setFilteredRestaurants(restaurants);
-    } else {
-      setFilteredRestaurants(filteredRestaurants);
+  const handleGenre = (e) => {
+    const genre = e.target.value;
+    setCurrentGenre(genre);
+    resetFilters('genres', genre);
+  };
+
+  const resetFilters = (filter, value) => {
+    let newFilteredRestaurantList = restaurants;
+
+    if (filter === 'states' && value !== 'ShowAll') {
+      newFilteredRestaurantList = newFilteredRestaurantList.filter((restaurant) => {
+        return restaurant.state === value;
+      });
+    } else if (filter !== 'states' && currentState !== 'ShowAll') {
+      newFilteredRestaurantList = newFilteredRestaurantList.filter((restaurant) => {
+        return restaurant.state === currentState;
+      });
     }
+
+    if (filter === 'genres' && value !== 'ShowAll') {
+      newFilteredRestaurantList = newFilteredRestaurantList.filter(
+        (restaurant) => {
+          return restaurant.genre.indexOf(value) >= 0;
+        }
+      );
+    } else if (filter !== 'genres' && currentGenre !== 'ShowAll') {
+      newFilteredRestaurantList = newFilteredRestaurantList.filter(
+        (restaurant) => {
+          return restaurant.genre.indexOf(currentGenre) >= 0;
+        }
+      );
+    }
+
+    setFilteredRestaurants(newFilteredRestaurantList);
   };
 
   return (
     <div className='App'>
       <div className='title'>Restaurants</div>
-      <Filter
-        instruction={'Show All States'}
-        listValues={config.state}
-        handleChange={handleStateAbbr}
-      />
-    {filteredRestaurants.length === 0 ? <EmptyGrid /> : <Grid header={config.header} data={filteredRestaurants}/>}
+      <div className='filter-container'>
+        <Filter
+          name='filter'
+          instruction='Show All Genres'
+          listValues={genres}
+          handleChange={handleGenre}
+        />
+        <Filter
+          name='states'
+          instruction='Show All States'
+          listValues={config.state}
+          handleChange={handleStateAbbr}
+        />
+      </div>
+      {filteredRestaurants.length === 0 ? (
+        <EmptyGrid />
+      ) : (
+        <Grid header={config.header} data={filteredRestaurants} />
+      )}
     </div>
   );
 };
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Filter = ({ instruction, listValues, handleChange }) => {
+const Filter = ({ name, instruction, listValues, handleChange }) => {
   return (
     <div className='filter'>
-      <select className='select' onChange={handleChange} >
+      <select id={name} className='select' onChange={handleChange} >
         <option value="ShowAll">{instruction}</option>
         {listValues.map((value) => (
           <option key={value} value={value}>{value}</option>
